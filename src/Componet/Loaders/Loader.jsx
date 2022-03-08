0dc417198eb14cbe942e200ac0e1af0d1cb95efd
theerudito <erudito.tv@gmail.com>
2022-03-08 17:06:30 -0500

    v1

---
+++ src/Componet/Loaders/Loader.jsx
@@ -0,0 +1,5 @@
+import MyLoader from "../Loaders/bloque_50px.svg";
+
+export const LoaderApp = () => {
+	return <img src={MyLoader} alt="logo" />;
+};
--- src/Componet/Page.jsx
+++ src/Componet/Page.jsx
@@ -3,106 +3,161 @@ import { SearchCountry, SearchCountryAlL } from './ApiData'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "./styles/style.css"
 
+import { LoaderApp } from "./Loaders/Loader";
+import Imagen from "./Image/image.png";
 
 const Page = () => {
-  const [Search, setSearch] = useState([])
-  const [country, setCountry] = useState([])
+	const [Search, setSearch] = useState([]);
+	const [country, setCountry] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [bandera, setBandera] = useState(true);
+	const [continente, setContinente] = useState(true);
+	const [paises1, setPaises1] = useState(true);
+	const [noBuscar, setNoBuscar] = useState(false);
+	const [error, setError] = useState(null);
 
+	const CountryAll = async () => {
+		const data = await SearchCountryAlL();
+		let paises = {
+			//Flag: data.countryInfo.flag,
+			Populations: data.population,
+			Country: data.affectedCountries,
+			Active: data.active,
+			Cases: data.cases,
+			CasesConfirmToday: data.todayCases,
+			Deaths: data.deaths,
+			DeathsToday: data.todayDeaths,
+			CasesCritical: data.critical,
+			Tests: data.tests,
+			RecoveredToday: data.todayRecovered,
+			CasesRecovered: data.recovered
+		};
+		setCountry(paises);
+	};
 
-  const CountryAll = async (props) => {
-      const data = await SearchCountryAlL()
-      let paises = {
-        //Flag: data.countryInfo.flag,
-        Populations: data.population,
-        Country: data.affectedCountries,
-        Continet: data.active,
-        Cases: data.cases,
-        CasesConfirmToday: data.todayCases,
-        Deaths: data.deaths,
-        DeathsToday: data.todayDeaths,
-        CasesCritical: data.critical,
-        Tests: data.tests,
-        RecoveredToday: data.todayRecovered,
-        CasesRecovered: data.recovered,
+	const Country = async () => {
+		setBandera(false);
+		setPaises1(false);
+		setContinente(false);
+		setLoading(false);
+		setNoBuscar(false);
+		const data = await SearchCountry(Search.trim());
+		let pais = {
+			Flag: data.countryInfo.flag,
+			Populations: data.population,
+			Country: data.country,
+			Continet: data.continent,
+			Cases: data.cases,
+			CasesConfirmToday: data.todayCases,
+			Deaths: data.deaths,
+			DeathsToday: data.todayDeaths,
+			CasesCritical: data.critical,
+			Tests: data.tests,
+			RecoveredToday: data.todayRecovered,
+			CasesRecovered: data.recovered
+		};
 
-      }
-      setCountry(paises)
-  }
+		setCountry(pais);
+	};
 
-  
-  const Country = async () => {
-    const data = await SearchCountry(Search)
-      let pais = {
-        Flag: data.countryInfo.flag,
-        Populations: data.population,
-        Country: data.country,
-        Continet: data.continent,
-        Cases: data.cases,
-        CasesConfirmToday: data.todayCases,
-        Deaths: data.deaths,
-        DeathsToday: data.todayDeaths,
-        CasesCritical: data.critical,
-        Tests: data.tests,
-        RecoveredToday: data.todayRecovered,
-        CasesRecovered: data.recovered,
+	const onChange = (e) => {
+		setSearch(e.target.value);
+		setNoBuscar(true);
+	};
 
-      }
-      setCountry(pais)     
-  }
+	// if (data.trim() === "") {
+	// 	setError("Debes Escribir Un Pais");
+	// 	return;
+	// }
 
-  const onChange = (e) => {
-    setSearch(e.target.value)
-  }
+	// // setTimeout(() => {
+	// // 	setError(null);
+	// // }, 2000);
 
+	useEffect(() => {
+		CountryAll();
+		setBandera(true);
+		setContinente(true);
+		setLoading(false);
+	}, []);
 
- 
+	return (
+		<Fragment>
+			{/*INPUT*/}
+			<div className="form-control container" id="contenedor">
+				<div className="w-50">
+					<input
+						id="input"
+						type="text"
+						placeholder="Search Country"
+						onChange={onChange}
+						name="input"
+						className="form-control m-4"
+					/>
+				</div>
 
-useEffect(() =>  {
-  CountryAll()
-},[])
+				{error && <div className="alert alert-danger mt-2">{error}</div>}
 
-  return (
-    <Fragment>
-          <div className="form-control container" id="contenedor">
-            <div className='w-50'>
-              <input 
-                  id="input"
-                  type="text" 
-                  placeholder='Search Country'
-                  onChange={onChange}
-                  className="form-control m-4"
-                  />
-            </div>
+				{/*BOTON BUSCAR*/}
+				<div>
+					{noBuscar ? (
+						<button className="btn btn-primary" id="boton" onClick={Country}>
+							Buscar
+						</button>
+					) : (
+						<button className="btn btn-primary " id="boton">
+							Buscar
+						</button>
+					)}
+				</div>
 
-                <div>
-                   <button className='btn btn-primary ' id="boton" onClick={Country} >Buscar</button>
-                </div>
-                <img src={country.Flag} id="flag"></img>
+				<div>
+					{/*PREGUNTA DE BANDERA*/}
+					{bandera ? (
+						<img src={Imagen} alt="bandera" id="flag"></img>
+					) : (
+						<img src={country.Flag} alt="bandera" id="flag"></img>
+					)}
+				</div>
 
-                
-                <div id="estadistic">
-                <p id="populations">Poblacion Total: {country.Populations}</p>
-                <p id="country" onChange={onChange}>Pais: {country.Country}</p>
-                
-                
+				{/*PREGUNTA DE LOADER*/}
+				{loading ? (
+					<LoaderApp />
+				) : (
+					<div id="estadistic">
+						<p id="populations">Poblacion Total: {country.Populations}</p>
 
-                <p id="continent">Continete: {country.Continet}</p>
-                <p id="cases">Casos Totales: {country.Cases}</p>
-                <p id="caseconfirm">Casos Confirmados Hoy: {country.CasesConfirmToday}</p>
-                <p id="deaths">Total de Muertes: {country.Deaths}</p>
-                <p id="deathstoday">Total de Muertes Hoy: {country.DeathsToday}</p>
-                <p id="critical">Casos Criticos: {country.CasesCritical}</p>
-                <p id="tests">Total de Tests: {country.Tests}</p>   
-                <p id="recoveredtoday">Total de Recuperados Hoy: {country.RecoveredToday}</p>
-                <p id="recovered">Total De Recuperados: {country.CasesRecovered}</p>
-                </div>
-            </div>
-    </Fragment>
-  )
-}
+						{/*PREGUNTA DE PAISES*/}
+						{paises1 ? (
+							<p id="country">Paises Contagiados: {country.Country}</p>
+						) : (
+							<p id="country">Pais: {country.Country}</p>
+						)}
 
-export default Page
+						{/*PREGUNTA DE PAISES*/}
+						{continente ? (
+							<p id="continent">Casos Activos: {country.Active}</p>
+						) : (
+							<p id="continent">Continete: {country.Continet}</p>
+						)}
 
-const cambio  = (props) => {
-  return <p>{props.mensaje}</p>
-}
+						<p id="cases">Casos Totales: {country.Cases}</p>
+						<p id="caseconfirm">
+							Casos Confirmados Hoy: {country.CasesConfirmToday}
+						</p>
+						<p id="deaths">Total de Muertes: {country.Deaths}</p>
+						<p id="deathstoday">Total de Muertes Hoy: {country.DeathsToday}</p>
+						<p id="critical">Casos Criticos: {country.CasesCritical}</p>
+						<p id="tests">Total de Tests: {country.Tests}</p>
+						<p id="recoveredtoday">
+							Total de Recuperados Hoy: {country.RecoveredToday}
+						</p>
+						<p id="recovered">Total De Recuperados: {country.CasesRecovered}</p>
+					</div>
+				)}
+			</div>
+		</Fragment>
+	);
+};
+
+export default Page;
